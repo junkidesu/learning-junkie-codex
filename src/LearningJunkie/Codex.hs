dde30857c9280119761c4f30453a302a18856be6
Anwar Sh. <alvaro.sh03@gmail.com>
2025-10-19 16:53:19 +0500

    Minor refactoring

--- src/LearningJunkie/Codex.hs
+++ src/LearningJunkie/Codex.hs
@@ -9,69 +9,29 @@ import System.Exit (ExitCode (ExitFailure, ExitSuccess))
 import System.Process
 
 executeCode :: Environment -> String -> IO ExecutionResult
-executeCode Node program = do
+executeCode environment program = do
         randomFileName <- toString <$> nextRandom
 
-        let temporaryFilePath = "node/" ++ randomFileName ++ ".js"
+        let
+                temporaryFilePath = case environment of
+                        Node -> "node/" ++ randomFileName ++ ".js"
+                        Python -> "python/" ++ randomFileName ++ ".py"
+                        Haskell -> "haskell/" ++ randomFileName ++ ".hs"
+                dockerFileLocation = case environment of
+                        Node -> "node/Dockerfile"
+                        Python -> "python/Dockerfile"
+                        Haskell -> "haskell/Dockerfile"
 
         writeFile temporaryFilePath program
 
         callCommand $
-                "docker build -f node/Dockerfile -t "
+                "docker build -f "
+                        ++ dockerFileLocation
+                        ++ " -t "
                         ++ randomFileName
                         ++ " --build-arg filepath="
                         ++ temporaryFilePath
-                        ++ "  ."
-
-        _processOutput@(processExitCode, stdOut, stdErr) <-
-                readCreateProcessWithExitCode
-                        (shell $ "docker run --rm " ++ randomFileName ++ ":latest")
-                        ""
-
-        callCommand $ "rm " ++ temporaryFilePath
-        callCommand $ "docker image rm " ++ randomFileName
-
-        return $ case processExitCode of
-                ExitSuccess -> Success $ pack stdOut
-                ExitFailure _ -> Failure $ pack stdErr
-executeCode Haskell program = do
-        randomFileName <- toString <$> nextRandom
-
-        let temporaryFilePath = "haskell/" ++ randomFileName ++ ".hs"
-
-        writeFile temporaryFilePath program
-
-        callCommand $
-                "docker build -f haskell/Dockerfile -t "
-                        ++ randomFileName
-                        ++ " --build-arg filepath="
-                        ++ temporaryFilePath
-                        ++ "  ."
-
-        _processOutput@(processExitCode, stdOut, stdErr) <-
-                readCreateProcessWithExitCode
-                        (shell $ "docker run --rm " ++ randomFileName ++ ":latest")
-                        ""
-
-        callCommand $ "rm " ++ temporaryFilePath
-        callCommand $ "docker image rm " ++ randomFileName
-
-        return $ case processExitCode of
-                ExitSuccess -> Success $ pack stdOut
-                ExitFailure _ -> Failure $ pack stdErr
-executeCode Python program = do
-        randomFileName <- toString <$> nextRandom
-
-        let temporaryFilePath = "python/" ++ randomFileName ++ ".py"
-
-        writeFile temporaryFilePath program
-
-        callCommand $
-                "docker build -f python/Dockerfile -t "
-                        ++ randomFileName
-                        ++ " --build-arg filepath="
-                        ++ temporaryFilePath
-                        ++ "  ."
+                        ++ " ."
 
         _processOutput@(processExitCode, stdOut, stdErr) <-
                 readCreateProcessWithExitCode
